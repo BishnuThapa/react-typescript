19f31a19ebf2d9f9ae4d4ea2e6ca8f158ff5c66c
Bishnu <thapabishnu20@gmail.com>
2023-12-25 16:45:14 +0545

    add active class using useState hook

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,14 +1,25 @@
+import { MouseEvent, useState } from "react";
 
+const handleClick = (event:MouseEvent) => console.log(event);
 
 function ListGroup() {
-    const items=["Kathmandu","Pokhara","Chitwan","Butwal","Nepalgunj"]
+    const items = ["Kathmandu", "Pokhara", "Chitwan", "Butwal", "Nepalgunj"]
+    const [selectedIndex, setSelectedIndex]=useState(-1)
     return (
       <>
-            <h2>List</h2>
-            { items.length==0 && <p>No Items Found</p>}
-            <ul className="list-group">
-                {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
-          
+        <h2>List</h2>
+        {items.length == 0 && <p>No Items Found</p>}
+        <ul className="list-group">
+          {items.map((item, index) => (
+            <li
+              key={item}
+              className={
+                selectedIndex == index ? "list-group-item active" : "list-group-item"             }
+              onClick={()=>setSelectedIndex(index)}
+            >
+              {item}
+            </li>
+          ))}
         </ul>
       </>
     );
